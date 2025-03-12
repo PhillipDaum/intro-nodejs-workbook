c0670748729f3b928d42cf368951fe399cc5a008
PhillipDaum <philgdaum@gmail.com>
2025-03-12 14:44:54 -0600

    add comments

--- 4-crud-code-along/index-3rd-party.js
+++ 4-crud-code-along/index-3rd-party.js
@@ -1,6 +1,7 @@
 // Create a program that checks to see if the current year is leap year using the Moment module.
+// This is an older module so I did it this way 
 const moment = require('moment'); // require
-const isCurrentYearLeapYear = moment(moment().year()).isLeapYear()
+const isCurrentYearLeapYear = moment(moment().year()).isLeapYear();
 if (isCurrentYearLeapYear) {
     console.log("It is a leap year.")
 } else {
